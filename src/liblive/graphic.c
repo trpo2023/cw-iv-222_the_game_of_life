34e97d876fee77863ddc3505de9b1eb61ce3f39a
Soskin Andrey <arthas@-.localdomain>
2023-04-24 22:15:58 +0700

    add print_field (Выводит поле) #6

--- src/liblive/graphic.c
+++ src/liblive/graphic.c
@@ -7,13 +7,27 @@ double wtime() {
   return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
 }
 
-
+// Вывод игрового поля
+void print_field(Grid grid)
+{
+    double start = wtime();
+    printf("Hello World %d %d %p\n", grid.height, grid.width, grid.field);
+    for (uint32_t i = 0; i < grid.height; i++) {
+        for (uint32_t j = 0; j < grid.width; j++) {
+            printf("%d ", grid.field[i][j]);
+        }
+        printf("\n");
+    }
+    double time = wtime() - start;
+    printf("\t\t\tВремя вывода игрового поля: %lf\n", time);
+}
+   
 //Количество живых клеток на игровом поле
 int get_live_count(Grid grid)
 {
     int count = 0;
-    for (int i = 0; i < __WORLD_WIDTH__; i++) {
-        for (int j = 0; j < __WORLD_HEIGHT__; j++) {
+    for (uint32_t i = 0; i < grid.height; i++) {
+        for (uint32_t j = 0; j < grid.width; j++) {
             if (grid.field[i][j] == 1) {
                 count++;
             }
--- src/liblive/graphic.h
+++ src/liblive/graphic.h
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/time.h>
+#include <inttypes.h>
+#include "logic.h"
 double wtime();
 int get_live_count(Grid grid);
+void print_field(Grid grid);
