2b18e835b1d58f1f2fe6ca1b630851a24508181c
None <you@example.com>
2023-05-09 02:27:57 +0700

    Add reading of mouse and keyboard(#9)

--- src/live/live.c
+++ src/live/live.c
@@ -1,3 +1,4 @@
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,8 +6,56 @@
 #include <liblive/graphic.h>
 #include <liblive/logic.h>
 
+#define KEY_ESC 27
+
 int main()
 {
+    // Инициализация ncurses
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    mousemask(ALL_MOUSE_EVENTS, NULL);
+
+    // Включение режима отображения координат мыши
+    mouseinterval(0);
+    curs_set(0);
+
+    int y, x;
+    // Цикл чтения событий мыши и клавиатуры
+    int ch;
+    MEVENT event;
+    while ((ch = getch()) != KEY_F(1)) {
+        switch (ch) {
+        case KEY_UP:
+            mvprintw(1, 0, "UP   ");
+            break;
+        case KEY_DOWN:
+            mvprintw(1, 0, "DOWN ");
+            break;
+        case '\n':
+            mvprintw(1, 0, "ENTER");
+            break;
+        case KEY_ESC:
+            mvprintw(1, 0, "ESC  ");
+            break;
+        case KEY_MOUSE:
+            getmouse(&event);
+            int x = event.x;
+            int y = event.y;
+            mvprintw(2, 0, "MOUSE: (%d %d)     ", x, y);
+            break;
+        default:
+            break;
+        }
+        getmaxyx(stdscr, y, x); // получаем размер окна (стандартный экран)
+        mvprintw(0, 0, "console size: %d %d     ", y, x); // выводим размер консоли
+    }
+
+    // Очистка ncurses и завершение программы
+    endwin();
+    return 0;
+    
     Grid grid = init_grid(35, 80);
     Grid tmp = init_grid(grid.rows, grid.columns);
     rand_grid(grid);
