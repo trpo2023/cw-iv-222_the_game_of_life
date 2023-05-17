faf3c271e0cab6acc479e93b25a166cd94ac557c
None <you@example.com>
2023-05-17 13:52:45 +0700

    Add main test(#7)

---
+++ test/graphic_test.c
@@ -0,0 +1,9 @@
+#include <liblive/graphic.h>
+
+#include <ctest.h>
+
+CTEST(GRAPHIC_TEST, test)
+{
+    ASSERT_EQUAL(0, 0);
+
+}
---
+++ test/logic_test.c
@@ -0,0 +1,9 @@
+#include <liblive/logic.h>
+
+#include <ctest.h>
+
+CTEST(LOGIC_TEST, test)
+{
+    ASSERT_EQUAL(0, 0);
+
+}
